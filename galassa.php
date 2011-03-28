639cf0b289c014031db963499679fc4f8352b7e6
Lee Braiden <lee.b@irukado.org>
2011-03-28 10:43:19 +0000

    Initial checkin

---
+++ galassa.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Plugin Name: Galassa
+Plugin URI: http://www.kintassa.com/projects/galassa
+Description: A flexible image gallery
+Version: 1.0
+Author: Lee Braiden
+Author URI: http://www.kintassa.com
+Copyright: Copyright (c) 2011 Kintassa. 
+License: All rights reserved.  Contact Kintassa should you wish to use this product.
+*/
+
+require_once("galassa_menu.php");
+$galassa_menu = new GalassaMenu("Galassa");
+
+?>
