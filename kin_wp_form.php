0b2ddf0a03bf822196b8e8df3db34bd881a50508
Lee Braiden <lee.b@irukado.org>
2011-04-04 14:00:33 +0000

    Initial work on standardised OOP Form API

--- kin_wp_form.php
+++ kin_wp_form.php
@@ -6,133 +6,256 @@ Copyright: Copyright (c) 2011 Kintassa.
 License: All rights reserved.  Contact Kintassa should you wish to use this product.
 */
 
-abstract class KintassaWPForm {
-	function KintassaWPForm($name) {
-		$this->name = $name;
-		$this->serial = "";
+abstract class KintassaFormElement {
+	const max_depth = 10;
+
+	function KintassaField() {
+		$this->parent = null;
 	}
 
-	function horiz_spacer() {
-		echo("&nbsp;");
+	function _set_parent($p) {
+		$this->parent = $p;
+	}
+
+	function parent_form() {
+		$count = 0;
+		$p = $this->parent;
+
+		while ($count < $max_depth) {
+			if (($p != null) && is_a($p, KintassaForm)) {
+				return $p;
+			}
+		}
+
+		// if we get here, there is no parent defined for the form
+		// field, so we SHOULD fail.
+		$p = null;
+		assert ($p == null);
+	}
+
+	/***
+	 * Since most fields accept whatever values are given, this
+	 * defaults to True.  MUST be overridden when further checks are
+	 * required.
+	 */
+	function is_valid($post_vars, $file_vars) {
+		return true;
+	}
+
+	abstract function render();
+}
+
+abstract class KintassaNamedFormElement extends KintassaFormElement {
+	function KintassaNamedFormElement($label, $name = null) {
+		$this->label = $label;
+
+		if ($name != null) {
+			$this->name = $this->name;
+		} else {
+			$label_as_name = KintassaNamedFormElement::label_to_name($label);
+		}
+	}
+
+	function name() {
+		return $this->form()->name() . "_" . $this->name;
+	}
+
+	static function label_to_name($label) {
+		$name = strtolower($label);
+		$name = str_replace(" ", "_", $name);
+		return $name;
+	}
+}
+
+abstract class KintassaField extends KintassaNamedFormElement {
+}
+
+class KintassaWPNonceField extends KintassaField {
+	function render() {
+		$action = -1;
+		$name = $this->name();
+		$referer = true;
+		$echo = false;
+		$html = wp_nonce_field($action, $name, $referer, $echo);
+
+		echo ($html);
+	}
+
+	function is_valid($post_vars, $file_vars) {
+		$name = $this->name();
+
+		if (wp_is_admin()) {
+			return check_admin_referer($name);
+		} else {
+			return wp_verify_nonce($name);
+		}
+	}
+}
+
+abstract class KintassaFieldContainer extends KintassaNamedFormElement {
+	function KintassaFieldContainer() {
+		$this->children = array();
+	}
+
+	function add_child($ch) {
+		$this->children.add($ch);
+		$ch->_set_parent($this);
+	}
+
+	function render() {
+		$this->begin_container();
+		$this->render_children();
+		$this->end_container();
+	}
+
+	function render_children() {
+		foreach ($this->children as $ch) {
+			$ch->render();
+		}
+	}
+
+	function is_valid($post_vars, $file_vars) {
+		foreach ($this->children as $ch) {
+			if (!$ch->is_valid($post_vars, $file_vars)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	abstract function begin_container();
+	abstract function end_container();
+}
+
+class KintasssaTextField {
+	function render() {
+		$name = $this->name();
+		echo("<div>");
+		echo("<label for=\"{$name}\">{$label}</label>");
+		echo("<input type=\"text\" name=\"{$name}\" value=\"\">");
+		echo("</div>");
+	}
+}
+
+class KintassaButton {
+	function render() {
+		$name = $this->name();
+		echo("<div>");
+		echo("<input type=\"submit\" value=\"{$label}\" name=\"{$btn_name}\">");
+		echo("</div>");
+	}
+}
+
+class KintassaNumberfield {
+	function render() {
+		$name = $this->name();
+		echo("<div>");
+		echo("<label for=\"{$name}\">{$label}</label>");
+		echo("<input type=\"text\" name=\"{$name}\" value=\"\">");
+		echo("</div>");
+	}
+
+	function is_valid($post_vars, $file_vars) {
+		$name = $this->
+		$val = $post_vars[$name];
+		return is_numeric($val);
+	}
+}
+
+class KintassaIntegerField {
+}
+
+class KintassaCheckbox {
+	function render() {
+		$name = $this->name();
+		echo("<div>");
+		echo("<label for=\"{$name}\">{$label}</label>");
+		echo("<input type=\"checkbox\" name=\"{$name}\" value=\"\">");
+		echo("</div>");
+	}
+}
+
+class KintassaRadioButton extends KintassaField {
+	function render() {
+		$name = $this->name();
+		// TODO: not implemented
+		assert (null);
+	}
+}
+
+class KintassaFileField extends KintassaField {
+	function render() {
+		$name = $this->name();
+		echo("<div>");
+		echo("<label for=\"{$name}\">{$label}</label>");
+		echo("<input type=\"file\" name=\"{$name}\" value=\"\">");
+		echo("</div>");
+	}
+
+	function is_valid($post_vars, $file_vars) {
+		return ($_FILE[$name]['error'] == UPLOAD_ERR_OK);
+	}
+}
+
+class KintassaRadioGroup extends KintassaFieldContainer {
+	function begin_container() {
+		// TODO: not implemented
+		assert(null);
+	}
+
+	function end_container() {
+		// TODO: not implemented
+		assert (null);
+	}
+}
+
+abstract class KintassaForm {
+	function KintassaForm($name) {
+		$this->name = $name;
 	}
 
 	function begin_form() {
-		$form_uri = $this->form_uri();
-		$form_name = $this->form_name();
-		echo "<form method=\"post\" action=\"{$form_uri}\" name=\"{$form_name}\">";
+		$form_uri = $this->uri();
+		$form_name = $this->name();
+		echo "<form method=\"post\" action=\"{$form_uri}\" name=\"{$form_name}\" enctype=\"multipart/form-data\">";
 	}
 
 	function end_form() {
 		echo "</form>";
 	}
 
-	function form_name() {
-		return "kin_frm_{$this->name}_{$this->serial}";
+	function name() {
+		return "kin_frm_{$this->name}";
 	}
 
-	function field_name($field) {
-		return $this->form_name() . "_" . $field;
-	}
-
-	function choose_name($label, $provided_name) {
-		if ($provided_name != null) {
-			$name = $provided_name;
-		} else {
-			$name = strtolower($label);
-			$name = str_replace(" ", "_", $name);
-		}
-		return $name;
-	}
-
-	function add_button($label, $name=null) {
-		$btn_name = $this->field_name($this->choose_name($label, $name));
-		echo("<div>");
-		echo("<input type=\"submit\" value=\"{$label}\" name=\"{$btn_name}\">");
-		echo("</div>");
-	}
-
-	function add_text_field($label, $name=null) {
-		$name = $this->field_name($this->choose_name($label, $name));
-		echo("<div>");
-		echo("<label for=\"{$name}\">{$label}</label>");
-		echo("<input type=\"text\" name=\"{$name}\" value=\"\">");
-		echo("</div>");
-	}
-
-	function add_number_field($label, $name=null) {
-		$name = $this->field_name($this->choose_name($label, $name));
-		echo("<div>");
-		echo("<label for=\"{$name}\">{$label}</label>");
-		echo("<input type=\"text\" name=\"{$name}\" value=\"\">");
-		echo("</div>");
-	}
-
-	function add_checkbox_field($label, $name=null) {
-		$name = $this->field_name($this->choose_name($label, $name));
-		echo("<div>");
-		echo("<label for=\"{$name}\">{$label}</label>");
-		echo("<input type=\"checkbox\" name=\"{$name}\" value=\"\">");
-		echo("</div>");
-	}
-
-	function begin_radio_field($label) {
-		$f = null;
-
-		// TODO: not implemented
-
-		return $f;
-	}
-
-	function add_radio_option($rf, $label, $name=null) {
-		$name = $this->field_name($this->choose_name($label, $name));
-		// TODO: not implemented
-	}
-
-	function end_radio_field($rf) {
-		// TODO: not implemented
-	}
-
+	/***
+	 * shortcut to test if a certain button has been submitted, without
+	 * building the entire form.
+	*/
 	function have_submission($btn) {
 		$real_btn_name = $this->field_name($btn);
 		$set = isset($_POST[$real_btn_name]);
 		return $set;
 	}
 
-	function form_uri() {
+	function uri() {
 		return esc_url($_SERVER['REQUEST_URI']);
 	}
 
-	abstract function execute();
+	function render() {
+		$this->begin_form();
+		foreach ($this->children as $ch) {
+			$ch->render();
+		}
+		$this->end_form();
+	}
 }
 
 /***
  * Represents a very simple, complete-and-submit style single-option form,
  * where the only submit button is simply called 'submit'.
  */
-abstract class KintassaWPSimpleForm {
-	abstract function generate_form();
-	abstract function process_results();
-
-	abstract function render_form_contents();
-
-	function render_form() {
-		$this->begin_form();
-		$this->render_form_contents();
-		$this->end_form();
-	}
-
-	function end_form() {
-		$this->render_button("Submit", _e("Submit"));
-		parent::end_form();
-	}
-
-	function execute() {
-		if ($this->have_submission('Submit')) {
-			$this->process_results();
-		} else {
-			$this->render_form();
-		}
-	}
+abstract class KintassaWPForm extends KintassaForm {
 }
 
 ?>
