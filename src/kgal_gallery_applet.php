7389bc1d3465fd4d27eb52271807e0dd7188b64b
Lee Braiden <lee.b@irukado.org>
2011-04-18 09:55:12 +0000

    Use  for applet registration to bypass inclusion scope issue

--- src/kgal_gallery_applet.php
+++ src/kgal_gallery_applet.php
@@ -10,31 +10,27 @@ require_once('kgal_config.php');
 require_once('kgal_image_finder.php');
 require_once(KGAL_ROOT_DIR . DIRECTORY_SEPARATOR . 'kintassa_core/kin_applet.php');
 
-$_registered_kintassa_gallery_applets = array();
+$GLOBALS['registered_kintassa_gallery_applets'] = array();
 
 abstract class KintassaGalleryApplet extends KintassaApplet {
 	static function register($applet_class, $name, $pretty_name) {
-		global $_registered_kintassa_gallery_applets;
-		if (array_key_exists($applet_class, $_registered_kintassa_gallery_applets)) return;
-		$_registered_kintassa_gallery_applets[$name] = array(
+		if (array_key_exists($applet_class, $GLOBALS['registered_kintassa_gallery_applets'])) return;
+		$GLOBALS['registered_kintassa_gallery_applets'][$name] = array(
 			'class'			=> $applet_class,
 			'pretty_name'	=> $pretty_name,
 		);
 	}
 
 	static function available_applets() {
-		global $_registered_kintassa_gallery_applets;
-		return array_keys($_registered_kintassa_gallery_applets);
+		return array_keys($GLOBALS['registered_kintassa_gallery_applets']);
 	}
 
 	static function is_valid_applet($applet_name) {
-		global $_registered_kintassa_gallery_applets;
-		return array_key_exists($applet_name, $_registered_kintassa_gallery_applets);
+		return array_key_exists($applet_name, $GLOBALS['registered_kintassa_gallery_applets']);
 	}
 
 	static function applet_info($applet_name) {
-		global $_registered_kintassa_gallery_applets;
-		return $_registered_kintassa_gallery_applets[$applet_name];
+		return $GLOBALS['registered_kintassa_gallery_applets'][$applet_name];
 	}
 
 	function __construct($gallery, $width=null, $height=null) {
